55c1ff460b02002b0fffd0fc1020c2baaeccb025
Justin Boxem <justinboxem@hotmail.com>
2017-11-02 04:51:43 +0100

    made edits
    Completed portfolio

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -3,20 +3,10 @@ include 'database.php';
 
 class Sponsor{
 	private $connection;
-	protected $sponsor_name;
-	protected $sponsor_image;
-	protected $sponsor_link;
 
 	public function __construct() {
         $this->connection = new DB_con();
     }
-
-//	public function create($name, $image, $link) {
-//			$this->sponsor_name = $name;
-//			$this->sponsor_image = $image;
-//			$this->sponsor_link = $link;
-//			return "Sponsor succesvol toegevoegd.";
-//		}
 
 	public function getDataFromDB() {
 			$array = array();
@@ -27,4 +17,22 @@ class Sponsor{
             }
             return $array;
 		}
+}
+
+class Portfolio{
+    private $connection;
+
+    public function __construct() {
+        $this->connection = new DB_con();
+    }
+
+    public function getPortfolioData() {
+            $array = array();
+            $query = "SELECT project, image, short FROM portfolio";
+            $result = $this->connection->sql($query);
+            while ($row = mysqli_fetch_assoc($result)) {
+                array_push($array, array("project"=>$row['project'], "image"=>$row['image'], "short"=>$row['short']));
+            }
+            return $array;
+        }
 }
