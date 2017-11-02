d5c7c4b84566d3b5204fd8fbae35dd4ed8161ce5
Justin Boxem <justinboxem@hotmail.com>
2017-11-02 19:30:31 +0100

    meep
    push 11/2/2017 7:30

--- events.php
+++ events.php
@@ -84,85 +84,16 @@
 
 
                       <div class="row" style="border-bottom: solid black 2px; background-color: lightblue; padding-top: 2px; padding-bottom: 2px; margin-bottom: 5px;">
-                        <div class="col-sm-1">
-                          <img class="img-responsive" src="example.jpg" alt="Chania" height="100px">
-                        </div>
                         <div class="col-sm-3">
-                          <h3 style="margin: 0px;">Event Naam</h3>
-                          Datum: 12-10-2017
+                          <img class="img-responsive" src="assets/images/bots.logo.png" alt="Chania" width="300px">
                         </div>
-                        <div class="col-sm-8">
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis diam ac mi suscipit dictum. Proin id sapien ac lectus venenatis placerat. Quisque lacinia eleifend lorem. Praesent suscipit ac diam eget ornare. Quisque ac eros et odio ornare suscipit nec placerat erat. Praesent a ligula sagittis, vestibulum ante et, euismod.
-                          </p>
+                        <div class="col-sm-4">
+                          <h3 style="margin: 0px;">Battle of the Schools</h3>
+                          Begindatum: 01-11-2017
+                          Einddatum: 03-11-2017
                         </div>
-                      </div>
-
-                      <div class="row" style="border-bottom: solid black 2px; background-color: lightblue; padding-top: 2px; padding-bottom: 2px; margin-bottom: 5px;">
-                        <div class="col-sm-1">
-                          <img class="img-responsive" src="example.jpg" alt="Chania" height="100px">
-                        </div>
-                        <div class="col-sm-3">
-                          <h3 style="margin: 0px;">Event Naam</h3>
-                          Datum: 12-10-2017
-                        </div>
-                        <div class="col-sm-8">
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis diam ac mi suscipit dictum. Proin id sapien ac lectus venenatis placerat. Quisque lacinia eleifend lorem. Praesent suscipit ac diam eget ornare. Quisque ac eros et odio ornare suscipit nec placerat erat. Praesent a ligula sagittis, vestibulum ante et, euismod.
-                          </p>
-                        </div>
-                      </div>
-
-                      <div class="row" style="border-bottom: solid black 2px; background-color: lightblue; padding-top: 2px; padding-bottom: 2px; margin-bottom: 5px;">
-                        <div class="col-sm-1">
-                          <img class="img-responsive" src="example.jpg" alt="Chania" height="100px">
-                        </div>
-                        <div class="col-sm-3">
-                          <h3 style="margin: 0px;">Event Naam</h3>
-                          Datum: 12-10-2017
-                        </div>
-                        <div class="col-sm-8">
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis diam ac mi suscipit dictum. Proin id sapien ac lectus venenatis placerat. Quisque lacinia eleifend lorem. Praesent suscipit ac diam eget ornare. Quisque ac eros et odio ornare suscipit nec placerat erat. Praesent a ligula sagittis, vestibulum ante et, euismod.
-                          </p>
-                        </div>
-                      </div>
-
-                      <div class="row" style="border-bottom: solid black 2px; background-color: lightblue; padding-top: 2px; padding-bottom: 2px; margin-bottom: 5px;">
-                        <div class="col-sm-1">
-                          <img class="img-responsive" src="example.jpg" alt="Chania" height="100px">
-                        </div>
-                        <div class="col-sm-3">
-                          <h3 style="margin: 0px;">Event Naam</h3>
-                          Datum: 12-10-2017
-                        </div>
-                        <div class="col-sm-8">
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis diam ac mi suscipit dictum. Proin id sapien ac lectus venenatis placerat. Quisque lacinia eleifend lorem. Praesent suscipit ac diam eget ornare. Quisque ac eros et odio ornare suscipit nec placerat erat. Praesent a ligula sagittis, vestibulum ante et, euismod.
-                          </p>
-                        </div>
-                      </div>
-
-                      <div class="row" style="border-bottom: solid black 2px; background-color: lightblue; padding-top: 2px; padding-bottom: 2px; margin-bottom: 5px;">
-                        <div class="col-sm-1">
-                          <img class="img-responsive" src="example.jpg" alt="Chania" height="100px">
-                        </div>
-                        <div class="col-sm-3">
-                          <h3 style="margin: 0px;">Event Naam</h3>
-                          Datum: 12-10-2017
-                        </div>
-                        <div class="col-sm-8">
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis diam ac mi suscipit dictum. Proin id sapien ac lectus venenatis placerat. Quisque lacinia eleifend lorem. Praesent suscipit ac diam eget ornare. Quisque ac eros et odio ornare suscipit nec placerat erat. Praesent a ligula sagittis, vestibulum ante et, euismod.
-                          </p>
-                        </div>
-                      </div>
-
-                      <div class="row" style="border-bottom: solid black 2px; background-color: lightblue; padding-top: 2px; padding-bottom: 2px; margin-bottom: 5px;">
-                        <div class="col-sm-1">
-                          <img class="img-responsive" src="example.jpg" alt="Chania" height="100px">
-                        </div>
-                        <div class="col-sm-3">
-                          <h3 style="margin: 0px;">Event Naam</h3>
-                          Datum: 12-10-2017
-                        </div>
-                        <div class="col-sm-8">
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis diam ac mi suscipit dictum. Proin id sapien ac lectus venenatis placerat. Quisque lacinia eleifend lorem. Praesent suscipit ac diam eget ornare. Quisque ac eros et odio ornare suscipit nec placerat erat. Praesent a ligula sagittis, vestibulum ante et, euismod.
+                        <div class="col-sm-5">
+                          <p>Tussen 1 en 3 november gaan 15 teams van verschillende scholen met elkaar de strijd aan voor de toekomst voor Groningen.
                           </p>
                         </div>
                       </div>
